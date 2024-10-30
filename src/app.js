f05a464511b52628aa3fe8a26f7326c9a3169823
Victor <victorsobierajski@gmail.com>
2024-10-30 09:21:35 -0300

    Users now can GET and UPDATE specific books.

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ const livros = [
     }
 ];
 
+function searchBook(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node.js');
 });
@@ -22,9 +28,20 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get('/livros/:id', (req, res) => {
+    const index = searchBook(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Book registered successfully.');
+});
+
+app.put('/livros/:id', (req, res) => {
+    const index = searchBook(req.params.id);
+    livros[index].title = req.body.title;
+    res.status(200).json(livros);
 });
 
 export default app
