38e1723a638d81ca4a9a04ee633f7df3d4e2780a
Victor <victorsobierajski@gmail.com>
2024-10-30 08:49:37 -0300

    Added the 'livros' route.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        'title': 'The Lord Of The Rings'
+    },
+    {
+        id: 2,
+        'title': 'The Hobbit'
+    }
+];
+
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node.js');
+});
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app
