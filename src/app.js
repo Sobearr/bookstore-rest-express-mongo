13a55fd0c674923d511d19bf03c1c3ff375a4cc9
Victor <victorsobierajski@gmail.com>
2024-11-05 14:32:41 -0300

    Refactor app.js to reflect new CRUD logic

--- src/app.js
+++ src/app.js
@@ -15,26 +15,4 @@ connection.once('open', () => {
 const app = express();
 routes(app);
 
-app.get('/livros/:id', (req, res) => {
-    const index = searchBook(req.params.id);
-    res.status(200).json(livros[index]);
-});
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Book registered successfully.');
-});
-
-app.put('/livros/:id', (req, res) => {
-    const index = searchBook(req.params.id);
-    livros[index].title = req.body.title;
-    res.status(200).json(livros);
-});
-
-app.delete('/livros/:id', (req, res) => {
-    const index = searchBook(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send(`Book with id ${index + 1} removed successfully.`);
-});
-
 export default app;
