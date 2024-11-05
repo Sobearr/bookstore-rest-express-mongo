221fd3dc0d2ef1267b5a9dfab9b6a0f54b8b8bb5
Victor <victorsobierajski@gmail.com>
2024-11-05 14:07:54 -0300

    Create listLivroById

--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -3,8 +3,22 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listLivros(req, res) {
-        const livros = await livro.find({});
-        res.status(200).json(livros);
+        try {
+            const livros = await livro.find({});
+            res.status(200).json(livros);
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - request error` });
+        }
+    }
+
+    static async listLivroById(req, res) {
+        try {
+            const id = req.params.id;
+            const foundLivro = await livro.findById(id);
+            res.status(200).json(foundLivro);
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - book request error` });
+        }
     }
 
     static async createLivro(req, res) {
