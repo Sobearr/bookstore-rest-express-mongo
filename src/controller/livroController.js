3564daf794b04940049b98ef723a4c724b3a314f
Victor <victorsobierajski@gmail.com>
2024-11-05 16:58:53 -0300

    Modified Livro and its logic to include Author

--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -1,4 +1,5 @@
 import livro from "../models/Livro.js";
+import { author } from "../models/Author.js";
 
 class LivroController {
 
@@ -22,9 +23,12 @@ class LivroController {
     }
 
     static async createLivro(req, res) {
+        const newLivro = req.body;
         try {
-            const newLivro = await livro.create(req.body);
-            res.status(201).json({ message: "Created successfully", livro: newLivro });
+            const authorFound = await author.findById(newLivro.author);
+            const livroComplete = { ...newLivro, author: { ...authorFound._doc }};
+            const livroCreated = await livro.create(livroComplete);
+            res.status(201).json({ message: "Created successfully", livro: livroCreated });
         } catch (error) {
             res.status(500).json({ message: `${error.message} - failed to create livro` });
         }
@@ -33,7 +37,14 @@ class LivroController {
     static async updateLivro(req, res) {
         try {
             const id = req.params.id;
-            await livro.findByIdAndUpdate(id, req.body);
+            if (req.body.author) {
+                const authorFound = await author.findById(req.body.author);
+                req.body.author = { ...authorFound._doc};
+                await livro.findByIdAndUpdate(id, req.body);
+            } else {
+                await livro.findByIdAndUpdate(id, req.body);
+            }
+            
             res.status(200).json({ message: 'Book updated'});
         } catch (error) {
             res.status(500).json({ message: `${error.message} - book update error` });
