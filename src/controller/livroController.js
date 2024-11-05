86b500233facd365cdeedfc0aca064bbf394b127
Victor <victorsobierajski@gmail.com>
2024-11-05 14:31:46 -0300

    Finish CRUD implementation for Livros

--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -28,7 +28,26 @@ class LivroController {
         } catch (error) {
             res.status(500).json({ message: `${error.message} - failed to create livro` });
         }
-        
+    }
+
+    static async updateLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: 'Book updated'});
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - book update error` });
+        }
+    }
+
+    static async deleteLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({ message: 'Book deleted.'});
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - book update error` });
+        }
     }
 
 };
