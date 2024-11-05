16aef7285dee79abbe9b091bc509dfc28690565e
Victor <victorsobierajski@gmail.com>
2024-11-05 12:38:46 -0300

    Updated get to now use db instead of list.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectToDatabase from './config/dbConnect.js';
+import livro from './models/Livro.js';
 
 const connection = await connectToDatabase();
 
@@ -14,29 +15,13 @@ connection.once('open', () => {
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        'title': 'The Lord Of The Rings'
-    },
-    {
-        id: 2,
-        'title': 'The Hobbit'
-    }
-];
-
-function searchBook(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    });
-}
-
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node.js');
 });
 
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
+app.get('/livros', async (req, res) => {
+    const livrosList= await livro.find({});
+    res.status(200).json(livrosList);
 });
 
 app.get('/livros/:id', (req, res) => {
