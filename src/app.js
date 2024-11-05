1037c24bf235a805ebe042ec62363008501ce69a
Victor <victorsobierajski@gmail.com>
2024-11-05 11:43:50 -0300

    Establish MongoDB connection.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import connectToDatabase from './config/dbConnect.js';
+
+const connection = await connectToDatabase();
+
+connection.on('error', (error) => {
+    console.error('connection error', error);
+});
+
+connection.once('open', () => {
+    console.log('DB connection successful.');
+});
 
 const app = express();
 app.use(express.json());
