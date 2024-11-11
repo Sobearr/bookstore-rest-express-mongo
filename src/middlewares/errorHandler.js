a73c3a31420c20c22700a14b4376898b91f835e9
Victor <victorsobierajski@gmail.com>
2024-11-11 14:05:21 -0300

    Create error-handling middleware

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,11 +3,16 @@ import mongoose from "mongoose";
 // eslint-disable-next-line no-unused-vars
 function errorHandler(error, req, res, next) {
   // console.error(error);
-  
+
   if (error instanceof mongoose.Error.CastError) {
     res.status(400).send({ message: "One or more data provided are incorrect." });
+  } else if (error instanceof mongoose.Error.ValidationError) {
+    const errorMessages = Object.values(error.errors)
+      .map(error => error.message)
+      .join("; ");
+    res.status(400).send({ message: `The following errors were found: ${errorMessages}` });
   } else {
-    res.status(500).send({ message: "Internal server error."});
+    res.status(500).send({ message: "Internal server error." });
   }
 }
 
