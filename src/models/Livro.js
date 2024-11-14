eb085f3d76f45ede0916e34a335bf453c643f427
Victor <victorsobierajski@gmail.com>
2024-11-14 14:09:34 -0300

    Added validation to the fields

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,12 +3,34 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId },
-  title: { type: String, required: [true, "The book's title is required"] },
-  publisher: { type: String, required: [true, "The publisher's name is required"] },
-  price: { type: Number },
-  pages: { type: Number },
-  // author: authorSchema
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "authors", required: [true, "The author's name is required"] }
+  title: {
+    type: String,
+    required: [true, "The book's title is required"]
+  },
+  publisher: {
+    type: String,
+    required: [true, "The publisher's name is required"],
+    enum: {
+      values: ["Allen", "Allen & Unwin"],
+      message: "The publisher {VALUE} is not allowed"
+    }
+  },
+  price: { type: Number },  
+  pages: {
+    type: Number,
+    validate: {
+      validator: (value) => {
+        return value >=10 && value <= 5000;
+      },
+      message: "Page number must be between 10 and 5000. Value inserted: {VALUE}"
+    },
+    required: [true, "Page number required"]
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "authors",
+    required: [true, "The author's name is required"]
+  }
 }, { versionKey: false });
 
 const livro = mongoose.model("livros", livroSchema);
