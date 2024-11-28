98be1185401ae385848b8bda904b47577add0b99
Victor <victorsobierajski@gmail.com>
2024-11-28 18:35:09 -0300

    Refactor specific routes to include pagination middleware

--- src/routes/authorsRoutes.js
+++ src/routes/authorsRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import AuthorController from "../controller/authorController.js";
+import pagination from "../middlewares/pagination.js";
 
 const router = express.Router();
 
 router
-  .get("/authors", AuthorController.listAuthors)
+  .get("/authors", AuthorController.listAuthors, pagination)
   .get("/authors/:id", AuthorController.listAuthorById)
   .post("/authors", AuthorController.createAuthor)
   .put("/authors/:id", AuthorController.updateAuthor)
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import LivroController from "../controller/livroController.js";
+import pagination from "../middlewares/pagination.js";
 
 const router = express.Router();
 
 router
-  .get("/livros", LivroController.listLivros)
-  .get("/livros/search", LivroController.listLivroByFilter)
+  .get("/livros", LivroController.listLivros, pagination)
+  .get("/livros/search", LivroController.listLivroByFilter, pagination)
   .get("/livros/:id", LivroController.listLivroById)
   .post("/livros", LivroController.createLivro)
   .put("/livros/:id", LivroController.updateLivro)
